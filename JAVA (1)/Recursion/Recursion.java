8bd574c8ae8b9b366c74948a4890a6aea6ad187a
Ashu Gupta <gaashu006@gmail.com>
2026-06-10 10:43:01 +0530

    Recursion

---
+++ JAVA (1)/Recursion/Recursion.java
@@ -0,0 +1,15 @@
+import java.util.*;
+//Recursion--
+
+public class Recursion {
+    static void p(int n) {
+        if(n==0){
+            return;
+        }
+        p(n-1);
+         System.out.println(n);
+    }
+    public static void main(String[] args) {
+        p(5);
+    }
+}
